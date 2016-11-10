742fb9acb19d1b1ece45582e5d66a63c54cdb0c2
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-11-11 01:41:53 +0700

    add tasks model with name, description, owner, due_date, section, project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110183742) do
+ActiveRecord::Schema.define(version: 20161110184111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20161110183742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "name"
+    t.text     "description"
+    t.integer  "owner_id"
+    t.datetime "due_date"
+    t.integer  "section_id"
+    t.integer  "project_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
