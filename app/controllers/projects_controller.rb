99b27039623a4d777dbf04ffd48ea82d94e31938
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-11-11 02:13:08 +0700

    Add create project and task

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,20 @@ class ProjectsController < ApplicationController
   def new 
     @project = Project.new
   end
+
+  def create
+    @project = Project.new project_params
+    
+    if @project.save
+      redirect_to root_path 
+    else
+      redirect_to new_project_path 
+    end
+
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,32 @@
 class TasksController < ApplicationController
+  require 'tasks_helper'
   def new 
     @project = Project.find(params[:project_id])
     @task = Task.new
+  end
+
+  def index
+
+  end
+
+  def create
+    load_project
+    @task = @project.tasks.build task_params
+    
+    if @task.save
+      redirect_to root_path 
+    else
+      redirect_to new_projects_task_path(@project) 
+    end
+
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name)
+  end
+
+  def load_project
+    @project = Project.find params[:project_id]
   end
 end
