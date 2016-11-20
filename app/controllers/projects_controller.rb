d95b1b6d6e198a64f7931a53ab3c7f4bb720a069
Thanh Khac <kingkid162@gmail.com>
2016-11-20 11:00:18 +0700

    Create project by modal

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 	def index
-
+		@project = Project.all.order("created_at DESC")
 	end
 
 	def new
@@ -9,27 +9,56 @@ class ProjectsController < ApplicationController
 
 	def create
 		@project = Project.new project_params
-
-		if @project.save
-			redirect_to root_path
-		else
-			redirect_to new_project_path
+		respond_to do |format|
+			if @project.save
+				format.html { redirect_to root_path }
+				format.json { head :no_content }
+				format.js
+			else
+				format.json { render json:@project.errors.full_messages,
+															status: :unprocessable_entity }
+			end
 		end
+	end
 
+	def edit
+		set_project
 	end
 
 	def show
-		@project = Project.find params[:id]
+		set_project
 	end
 
 	def update
-
+		set_project
+		respond_to do |format|
+			if @project.update project_params
+				format.html { redirect_to project_path }
+				format.json { head :no_content }
+				format.js
+			else
+				format.json { render json: @project.errors.full_messages,
+															status: :unprocessable_entity }
+			end
+		end
 	end
 
 	def destroy
-
+		set_project
+		@project.destroy
+		respond_to do |format|
+			format.html { redirect_to project_path }
+			format.json { head :no_content }
+			format.js
+		end
 	end
+
 	private
+
+	def set_project
+		@project = Project.find params[:id]
+	end
+
 	def project_params
 		params.require(:project).permit(:name)
 	end
