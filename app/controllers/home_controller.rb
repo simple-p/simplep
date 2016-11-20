d4def0c4c50f119de7cfedc13f73504e8bdbde54
Toan Ngo <xhunter9x@gmail.com>
2016-11-20 11:08:03 +0700

    made homepage a bit better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index]
 	def index
+		if current_user
+			redirect_to projects_path
+		end
 	end
 	def dashboard
 		@teams = Team.all
