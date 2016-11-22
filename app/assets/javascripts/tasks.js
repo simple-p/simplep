6d38ed6c267d24b73d0a0405f2dfaa85f039cc8f
Manh T. Truong <manhattan.vn@gmail.com>
2016-11-22 19:14:04 +0700

    Ajax for task completed worknow ( need datatype .js in jquery request)

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,6 +1,6 @@
-function handleChange(el)
-{
+function handleChange(el) {
   console.log("URL: " + $(el).data('request-url')); 
   console.log("Completed: " + $(el).prop('checked'));
-  $.post($(el).data('request-url'), {completed: $(el).prop('checked')});
+  url = $(el).data('request-url') + '.js'
+  $.post(url, {completed: $(el).prop('checked')});
 }
