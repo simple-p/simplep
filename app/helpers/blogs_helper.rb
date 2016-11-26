08a4fa03e25724771d8af2171a9134ce4ed9688d
Toan Ngo <xhunter9x@gmail.com>
2016-11-26 18:04:10 +0700

    done edit, update, add blogs

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,6 @@
 module BlogsHelper
+  def statusIcon(status)
+    statusIcons = {"Ahead" => "fa fa-calendar-plus-o", "On Track" => "fa fa-calendar-check-o", "Late" => "fa fa-calendar-times-o"}
+    statusIcons[status.to_s]
+  end
 end
